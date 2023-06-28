e3a6013fdb8dba62fefc674842a268cc68e6b5e8
BarbaraNicoleR <bar.riffo@duocuc.cl>
2023-06-28 00:00:51 -0400

    juegos 5 6 7 8

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from.views import home,contacto,cartelera
+from.views import home,contacto,cartelera,juego5,juego6,juego7,juego8
 
 urlpatterns = [
     path('', home,name="home" ),
     path('contacto/', contacto,name="contacto" ),
     path('cartelera/', cartelera,name="cartelera" ),
+    path('juego5/', juego5,name="juego5" ),
+    path('juego6/', juego6,name="juego6" ),
+    path('juego7/', juego7,name="juego7" ),
+    path('juego8/', juego8,name="juego8" ),
+
+
 ]
